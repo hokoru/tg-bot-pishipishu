af6e2d9be5100cb4705e2a2e27a8e186146b80ca
hokoru <144522397+hokoru@users.noreply.github.com>
2026-03-03 00:35:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -234,8 +234,10 @@ async def materials(msg: Message, state: FSMContext):
 # ================= ЗАПУСК =================
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await set_commands(bot)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
